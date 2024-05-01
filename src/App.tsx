d6f146090e76cd6756026a2d455ac62f050eb69a
Nakamura <hhujk5@gmail.com>
2024-05-01 19:47:22 +0900

    ログインページを作成する

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,26 @@ import React from 'react';
 import './App.scss';
 import Sidebar from './components/sidebar/sidebar';
 import Chat from './components/chat/chat';
+import { useSelector } from 'react-redux';
+import Login from './components/login/login';
 
 function App() {
+  // user情報によってログインページを表示する
+  // const user = useSelector((state) => state.user.user);
+  const user = null; // TODO 一旦nullにしている
+
   return (
     <div className="App">
-      {/* sidebar */}
-      <Sidebar />
-
-      {/* chat */}
-      <Chat />
+      {user ? (
+        <>
+          <Sidebar />
+          <Chat />
+        </>
+      ) : (
+        <>
+          <Login />
+        </>
+      )}
     </div>
   );
 }
